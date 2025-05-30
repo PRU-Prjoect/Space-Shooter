1579418e4d9bcc1c4ba5e0945adcad907698b8c6
Đông Nghi <hoangvodonghi@gmail.com>
2025-05-30 12:31:32 +0700

    update

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
 
-public class Background: MonoBehaviour
+public class Background : MonoBehaviour
 {
-    
+    Vector3 MaxPoint;
+    Vector3 MinPoint;
+    void Start()
+    {
+        MaxPoint = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, Camera.main.pixelHeight, 0.0f));
+        MinPoint = Camera.main.ScreenToWorldPoint(Vector3.zero);
+        float WidthScreen = MaxPoint.x - MinPoint.x;
+        float HeightScreen = MaxPoint.y - MinPoint.y;
+        //
+        float pixelPerUnit = this.GetComponent<SpriteRenderer>().sprite.pixelsPerUnit;
+        Vector3 sizeOfSprite = new (this.GetComponent<SpriteRenderer>().sprite.rect.width, this.GetComponent<SpriteRenderer>().sprite.rect.height, 0.0f);
+        //
+        Vector3 sizeOfBackground = new (WidthScreen / (sizeOfSprite.x / pixelPerUnit), HeightScreen / (sizeOfSprite.y / pixelPerUnit), 0.0f);
+        this.transform.localScale = sizeOfBackground;
+    }
+
 }
+
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    float speed = 5.0f;
+    private Keyboard keyboard;
+
+    void Start()
+    {
+        keyboard = Keyboard.current;
+    }
+
+    void Update()
+    {
+        if (keyboard == null) return;
+
+        if (keyboard.spaceKey.wasPressedThisFrame)
+        {
+            // Xử lý khi nhấn Spacessssss
+        }
+
+        if (keyboard.wKey.isPressed)
+        {
+            this.transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+
+        if (keyboard.sKey.isPressed)
+        {
+            this.transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+
+        if (keyboard.aKey.isPressed)
+        {
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+        if (keyboard.dKey.isPressed)
+        {
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+    }
+}
