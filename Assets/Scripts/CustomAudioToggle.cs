c4e65e6f81a332fe78849a737dee1a1f8cbcfd8f
Đông Nghi <hoangvodonghi@gmail.com>
2025-06-08 16:20:42 +0700

    update start scene

---
+++ Assets/Scripts/CustomAudioToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomAudioToggle : MonoBehaviour
+{
+    [SerializeField] private Button toggleButton;
+    [SerializeField] private Sprite soundOnSprite;
+    [SerializeField] private Sprite soundOffSprite;
+
+    private bool isSoundOn = true;
+
+    void Start()
+    {
+        toggleButton.onClick.AddListener(ToggleSound);
+        UpdateButtonSprite();
+    }
+
+    void ToggleSound()
+    {
+        isSoundOn = !isSoundOn;
+        AudioListener.volume = isSoundOn ? 1f : 0f;
+        UpdateButtonSprite();
+    }
+
+    void UpdateButtonSprite()
+    {
+        toggleButton.GetComponent<Image>().sprite = isSoundOn ? soundOnSprite : soundOffSprite;
+    }
+}
---
+++ Assets/Scripts/PlaneSelectionPopup.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlaneSelectionPopup : MonoBehaviour
+{
+    [SerializeField] private GameObject popupPanel;
+    [SerializeField] private Button optionButton;
+    [SerializeField] private Button closeButton;
+    [SerializeField] private Button[] planeButtons;
+    [SerializeField] private GameObject[] planeModels;
+
+    void Start()
+    {
+        optionButton.onClick.AddListener(OpenPopup);
+        closeButton.onClick.AddListener(ClosePopup);
+
+        // Gắn sự kiện cho từng nút máy bay
+        for (int i = 0; i < planeButtons.Length; i++)
+        {
+            int index = i;
+            planeButtons[i].onClick.AddListener(() => SelectPlane(index));
+        }
+
+        popupPanel.SetActive(false);
+    }
+
+    void OpenPopup()
+    {
+        popupPanel.SetActive(true);
+        Time.timeScale = 0f; // Tạm dừng game
+    }
+
+    void ClosePopup()
+    {
+        popupPanel.SetActive(false);
+        Time.timeScale = 1f; // Tiếp tục game
+    }
+
+    void SelectPlane(int index)
+    {
+        // Ẩn tất cả máy bay
+        foreach (GameObject plane in planeModels)
+        {
+            plane.SetActive(false);
+        }
+
+        // Hiện máy bay được chọn
+        planeModels[index].SetActive(true);
+
+        ClosePopup();
+        Debug.Log($"Đã chọn máy bay {index}");
+    }
+}
